69fdbdb2a44168815920b88f52bc32a7831d58dd
narf200 <narf200@gmail.com>
2020-10-17 23:11:40 +0500

    refactoring Dialogs component added 2 columns dialogs and messages

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -4,8 +4,19 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props)=> {
   return (
-    <div className={s.DialogsItem}>
-        Message 1
+    <div className={s.dialogsItem}>
+        <div className={s.dialogs}>
+            <div className={s.dialog}>Sandy</div>
+            <div className={s.dialog}>Motya</div>
+            <div className={s.dialog}>Fil</div>
+            <div className={s.dialog}>Barsik</div>
+        </div>
+        <div className={s.messages}>
+            <div className={s.message}>Give me food</div>
+            <div className={s.message}>Play whith me</div>
+            <div className={s.message}>I`m hungry</div>
+            <div className={s.message}>Pet my fur</div>
+        </div>
     </div>)
 }
 export default Dialogs
