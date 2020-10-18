7660e0d0d93c326ab659c80b904df8bbc190e79e
narf200 <narf200@gmail.com>
2020-10-18 15:48:05 +0500

    refactoring Dialog: added NavLink

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 
 const Dialogs = (props)=> {
   return (
     <div className={s.dialogsItem}>
         <div className={s.dialogs}>
-            <div className={s.dialog}>Sandy</div>
-            <div className={s.dialog}>Motya</div>
-            <div className={s.dialog}>Fil</div>
-            <div className={s.dialog}>Barsik</div>
+            <div className={s.dialog}>
+               <NavLink to='dialogs/1'>Sandy</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to='dialogs/2'>Motya</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to='dialogs/3'>Fil</NavLink>
+            </div>
+            <div className={s.dialog}>
+                <NavLink to='dialogs/4'>Barsik</NavLink>
+            </div>
         </div>
         <div className={s.messages}>
             <div className={s.message}>Give me food</div>
