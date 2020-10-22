cd891387e2fa87d2387dff7fc22e39062e57083b
narf200 <narf200@gmail.com>
2020-10-22 20:54:29 +0500

    create store

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {subscribe, addPost, updateNewPostText} from "./redux/state";
+import store from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
  let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>,document.getElementById('root')
     );
 }
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,62 +1,63 @@
-let rerenderEntireTree = () => {
-    console.log('qOqO')
-}
+ let store = {
+    _state: {
+        profilePage: {
+            postData: [
+                {id:'1', postText:'It`s my first post.', likesCount:'12'},
+                {id:'2', postText:'Where is my cat ?', likesCount:'8'},
+                {id:'3', postText:'Thanks for the new toys!', likesCount:'3'},
+                {id:'4', postText:'Where is my box ?', likesCount:'55'}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogsData: [
+                { id:'1', name:"Sandy"},
+                { id:'2', name:"Motya"},
+                { id:'3', name:"Fil"},
+                { id:'4', name:"Barsik"}
+            ],
 
-let state = {
-    profilePage: {
-         postData: [
-            {id:'1', postText:'It`s my first post.', likesCount:'12'},
-            {id:'2', postText:'Where is my cat ?', likesCount:'8'},
-            {id:'3', postText:'Thanks for the new toys!', likesCount:'3'},
-            {id:'4', postText:'Where is my box ?', likesCount:'55'}
-        ],
-        newPostText: ''
+            messagesData: [
+                {id:'1', message:"Give me food"},
+                {id:'2', message:"Play whith me"},
+                {id:'3', message:"I`m hungry"},
+                {id:'4', message:"Pet my fur"}
+            ]
+        },
+        sideBar: {
+            friendsList: [
+                { id:'1', name:"Sandy", avatar:"https://pbs.twimg.com/profile_images/980078612081795072/g_NUbXIw_400x400.jpg", status:'online'},
+                { id:'2', name:"Motya", avatar:"https://pbs.twimg.com/profile_images/1080545769034108928/CEzHCTpI_400x400.jpg", status:'online'},
+                { id:'3', name:"Fil", avatar:"https://pbs.twimg.com/profile_images/506215886500687873/09EWW0Sm_400x400.jpeg", status:'online'},
+                { id:'4', name:"Barsik", avatar:"https://pbs.twimg.com/profile_images/1057953961804738560/h6OFjm3E_400x400.jpg", status:'online'}
+            ]
+        },
     },
-    dialogsPage: {
-         dialogsData: [
-            { id:'1', name:"Sandy"},
-            { id:'2', name:"Motya"},
-            { id:'3', name:"Fil"},
-            { id:'4', name:"Barsik"}
-        ],
-
-         messagesData: [
-            {id:'1', message:"Give me food"},
-            {id:'2', message:"Play whith me"},
-            {id:'3', message:"I`m hungry"},
-            {id:'4', message:"Pet my fur"}
-        ]
+     getState(){
+        return this._state
+     },
+     _callSubscriber() {
+        console.log('qOqO')
     },
-    sideBar: {
-        friendsList: [
-            { id:'1', name:"Sandy", avatar:"https://pbs.twimg.com/profile_images/980078612081795072/g_NUbXIw_400x400.jpg", status:'online'},
-            { id:'2', name:"Motya", avatar:"https://pbs.twimg.com/profile_images/1080545769034108928/CEzHCTpI_400x400.jpg", status:'online'},
-            { id:'3', name:"Fil", avatar:"https://pbs.twimg.com/profile_images/506215886500687873/09EWW0Sm_400x400.jpeg", status:'online'},
-            { id:'4', name:"Barsik", avatar:"https://pbs.twimg.com/profile_images/1057953961804738560/h6OFjm3E_400x400.jpg", status:'online'}
-        ]
+    addPost() {
+        let newPost = {
+            id:'5',
+            postText: this._state.profilePage.newPostText,
+            likesCount:'24'
+        }
+        this._state.profilePage.postData.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newPostText) {
+        this._state.profilePage.newPostText = newPostText
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
     },
 }
 
-export let addPost = () => {
-    let newPost = {
-        id:'5',
-        postText: state.profilePage.newPostText,
-        likesCount:'24'
-    }
-    state.profilePage.postData.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
+export default store
 
-export let updateNewPostText = (newPostText) => {
-    state.profilePage.newPostText = newPostText
-    rerenderEntireTree(state)
-}
-
-export let subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
-
-export default state
-
-
+window.store = store
